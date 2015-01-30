ba4a66333fe8059961e67db1a342dd215c05e1ce
jmarrietar <jmarrietar@gmail.com>
2015-01-29 19:53:49 -0500

    jh

--- Classification_Trees/Classification_Trees.R
+++ Classification_Trees/Classification_Trees.R
@@ -47,5 +47,40 @@ text(model)
 #CROSS-VALIDATION#
 ##################
 
+#This is done multiple times to prevent variability
+
+#Create a subset on the training data 
+subset<-traindata[1:10,-7]
+train_subset<-traindata[11:150,]
+
+subset_two <- traindata[11:20, ]
+#Save the religions of subset into subset_religions. 
+
+subset_religions<-traindata[1:10,7]
+subset_religions2<-traindata[11:20, 7]
+
+#Build a model-> Build model on the rest of the training_Data
+cv_model<-rpart(religion~circles+crosses+saltires+quarters+sunstars+crescent+triangle,data=train_subset,method='class',control=rpart.control(minsplit=10))
+
+#Make Predictions on subset data. 
+cv_predictions<-predict(cv_model,newdata=subset,type='class')
+
+cv_predictions_two<-predict(cv_model,newdata=subset_two,type='class')
+
+cv_predictions<-as.vector(cv_predictions)
+
+cv_predictions_two<-as.vector(cv_predictions_two)
+
+#ACCURATE 
+# we cross-validate our results by performing matching and sum the boolean vector.
+sum(cv_predictions==subset_religions)
+
+sum(cv_predictions_two==subset_religions2)
+
+######################
+#Complexity Parameter#
+######################
+
+
 
 
